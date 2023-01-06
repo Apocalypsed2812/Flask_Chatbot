50fe07898c6928dc1ea48d33d8078214e780756b
Apocalysed <phamhuynhanhtien.12a20.2019@gmail.com>
2023-01-06 16:42:58 +0700

    train model with 20 epochs

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import re
 import numpy as np
 import pickle
 import keras
-import pandas as pd
 from underthesea import word_tokenize 
 from keras_preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
@@ -23,41 +22,53 @@ def clean_text(sent):
 def clean_and_word_segmentation(sent):
     return word_tokenize(clean_text(sent.lower()), format='text')
 
+# hàm để chuyển từ chuỗi sang token
 def str_to_tokens(sentence):
-    #words = sentence.lower().split()
     words = clean_and_word_segmentation(sentence).split()
     tokens_list = list()
+    flag = True
     for current_word in words:
+        if((current_word in tokenizer_word_index) == False):
+            print(current_word in tokenizer_word_index)
+            flag = False
+            break
         result = tokenizer_word_index[current_word]
         if result != '':
             tokens_list.append(result)
+    if(flag == False):
+        return False
     return pad_sequences([tokens_list], maxlen=maxlen_questions,padding='post')
 
+# hàm chatbot trả lời câu hỏi
 def chatbot(input):
     input_question = input
 
     if input_question == '':
-        return 'Give for me your question'
-    states_values = enc_model.predict(str_to_tokens(input_question))
-    empty_target_seq = np.zeros((1,1))
-    empty_target_seq[0,0] = tokenizer_word_index['<START>']
-    stop_condition = False
-    decoded_translation = ''
-    while not stop_condition:
-        dec_outputs, h, c = dec_model.predict([empty_target_seq]+states_values)
-        sampled_word_index = np.argmax(dec_outputs[0,-1, :])
-        sampled_word = None
-        for word, index in tokenizer_word_index.items():
-            if sampled_word_index == index:
-                if word != '<END>':
-                    decoded_translation += f'{word} '
-                sampled_word = word
-
-        if sampled_word == '<END>' or len(decoded_translation.split()) > maxlen_answers:
-            stop_condition = True
+        return 'Vui lòng nhập câu hỏi của bạn'
+    str_tokens = str_to_tokens(input_question)
+    if(type(str_tokens) == bool):
+        return 'Câu hỏi không hợp lệ'
+    else:
+        states_values = enc_model.predict(str_tokens)
         empty_target_seq = np.zeros((1,1))
-        empty_target_seq[0,0] = sampled_word_index
-        states_values = [h,c]
-    print('User: ',input_question)
-    print('Bot answer:', decoded_translation, '\n')
+        empty_target_seq[0,0] = tokenizer_word_index['<START>']
+        stop_condition = False
+        decoded_translation = ''
+        while not stop_condition:
+            dec_outputs, h, c = dec_model.predict([empty_target_seq]+states_values)
+            sampled_word_index = np.argmax(dec_outputs[0,-1, :])
+            sampled_word = None
+            for word, index in tokenizer_word_index.items():
+                if sampled_word_index == index:
+                    if word != '<END>':
+                        decoded_translation += f'{word} '
+                    sampled_word = word
+
+            if sampled_word == '<END>' or len(decoded_translation.split()) > maxlen_answers:
+                stop_condition = True
+            empty_target_seq = np.zeros((1,1))
+            empty_target_seq[0,0] = sampled_word_index
+            states_values = [h,c]
+        print('User: ',input_question)
+        print('Bot answer:', decoded_translation, '\n')
     return decoded_translation
